657b6923ff8c22f3bbe92c1424e8af1f0940ad51
madrimovshaxzod200-hub <madrimovshaxzod200@gmail.com>
2026-02-17 17:55:41 +0500

    Update user.py

--- bot/handlers/user.py
+++ bot/handlers/user.py
@@ -2,7 +2,7 @@ from aiogram import Router, F
 from aiogram.types import Message, CallbackQuery
 from aiogram.fsm.context import FSMContext
 
-from database import db
+from bot.database import db
 from keyboards.user_kb import main_menu, sections_keyboard, units_keyboard
 from states import BookingStates
 from filters import NotBlocked
